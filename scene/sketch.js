c433eebb1e96b5c09751bbb9fa64c0868ae982c1
BurntBeefBurrito <gorylevmichael@gmail.com>
2024-03-27 14:12:19 -0600

    I figured out object notation :D

--- scene/sketch.js
+++ scene/sketch.js
@@ -1,7 +1,7 @@
 // Rhythm Heaven't
 // Michael Gorylev
-// 3/12/24
-//       offset text
+// 3/27/24
+//        offset text
 // Extra for Experts:
 // - I used arrays before they were introduced (3/11/24)
 
@@ -11,7 +11,7 @@
 //images idk
 let boom;
 let bumperImage;
-let arrowImage;
+let bumperDownImage;
 
 //variables relating to notes and lanes
 let lanes; // the number of lanes lol, dont use more than 4 yet
@@ -25,6 +25,7 @@ let noteTraits = []; //the updated lane and distance array
 //misc variables
 let state; // is it in a menu, or playing?
 let binds; //keybindings the user presses
+let frameser; //frames was taken, this just counts frames                                          DO THISS GRAAAAHH
 
 //visual variables
 let offsetx; //how offset are the lanes and things? This will be used for silly mechanics when i get around to them
@@ -34,19 +35,19 @@ let rotation; //i legit have no idea how I'll do this
 
 //                                                     USE OBJECT NOTATION
 //                                                     Maybe for mapping? setting notespeeds and kps'?
-//                                                     use for notetraits (dist, lane)
+//                                                     used for notetraits (speed, distance, lane)
 //picture heaven
 function preload(){
   bumperImage = loadImage("images/Slingshotwoah.jpg");
   boom = loadImage("images/Boomboxgg.jpg");
-  arrowImage = loadImage("images/SlingshotGoodJob.jpg");
+  bumperDownImage = loadImage("images/SlingshotGoodJob.jpg");
 }
 
 function setup() {
   angleMode(DEGREES);
-  lanes = 4;
+  lanes = 6;
   createCanvas(windowWidth, windowHeight);
-  state = "play";
+  state = "menu";
   binds = [68, 70, 74, 75, 83, 76];
   accuracy = 200;
   noteSpeed = 6;
@@ -57,7 +58,7 @@ function setup() {
   let tempNote = {
     speed: noteSpeed,
     lane: round(random(0, lanes-1)),
-    distance: round(windowHeight*0.8 - 800),
+    distance: round(windowHeight + 100),
 
   };
   noteTraits.push(tempNote);
@@ -65,9 +66,7 @@ function setup() {
 
 function draw() {
   background(220, 150, 220);
-  //                                      THIS IS FOR ON SCREEN TEXT FEEL FREE TO CHANGE FOR TESTS
-  text(noteTraits[noteTraits.length-1].lane, 20, 20);
-  text(noteDistance, 20, 50);
+  
   
   if (state === "play"){
     playing();
@@ -78,23 +77,32 @@ function draw() {
 }
 
 function menu(){ //only here because a menu is required :skull: also i dont wanna make this bc it makes testing slower
-  //TBD
+  text("This is a very high budget menu. hit left click to play the thing", windowWidth/2 - 150, windowHeight/2);
+  if (mouseIsPressed){
+    state = "play";
+
+  }
 }
 
 function playing(){ //this is the ceo of the thing being playable
   bumperMan();
   arrowMan();
   candyMan();
+  spawnNote();
+
+  //                                      THIS IS FOR ON SCREEN TEXT FEEL FREE TO CHANGE FOR TESTS
+  text(noteTraits.length, 20, 20);
+  text(noteDistance, 20, 50);
 }
 
 function bumperMan(){ //this draws the bumpers
   for(let i = 0; i < lanes; i++){
 
-    image(bumperImage, windowWidth/2-spacing/2 + spacing / lanes * i + offsetx + spacing/2/lanes, windowHeight*0.8, 60, 60);
-
     if (keyIsDown(binds[i])){
-      image(arrowImage, windowWidth/2-spacing/2 + spacing / lanes * i + offsetx + spacing/2/lanes, windowHeight*0.8, 60, 60);
-      
+      image(bumperDownImage, windowWidth/2-spacing/2 + spacing / lanes * i + offsetx + spacing/2/lanes, windowHeight*0.8, 60, 60);
+    }
+    else{
+      image(bumperImage, windowWidth/2-spacing/2 + spacing / lanes * i + offsetx + spacing/2/lanes, windowHeight*0.8, 60, 60);
     }
   }
 }
@@ -103,12 +111,12 @@ function keyPressed(){ //this deletes notes around a bumper when its pressed
   for(let q = 0; q < lanes; q++){
     if (keyCode === binds[q]){
 
-      for(let possibleNotes = 0; possibleNotes < noteDistance.length; possibleNotes++){
+      for(let possibleNotes = 0; possibleNotes < noteTraits.length; possibleNotes++){ 
 
-        if (noteDistance[possibleNotes] >= windowHeight*0.8 - accuracy && noteDistance[possibleNotes] 
-          <= windowHeight*0.8 + accuracy && noteLane[possibleNotes] === q){
-          noteDistance.splice(possibleNotes, 1); //shorten prev line plz and make this more consistent since it sometimes misses notes
-          noteLane.splice(possibleNotes, 1);
+        if (noteTraits[possibleNotes].distance >= windowHeight*0.8 - accuracy && noteTraits[possibleNotes].distance 
+          <= windowHeight*0.8 + accuracy && noteTraits[possibleNotes].lane === q){
+          noteTraits.splice(possibleNotes, 1);
+          possibleNotes = noteTraits.length+40;
         }
       }
     }
@@ -116,27 +124,17 @@ function keyPressed(){ //this deletes notes around a bumper when its pressed
 }
 function arrowMan(){ //this is an all you can eat buffet for note management
 
-  // if (keyIsDown(32)){ //this creates notes OLDDDD
-  //   append(noteDistance, round(windowHeight*0.8 - 800));
-  //   append(noteLane, ));
-  // }
-
-  spawnNote();
-
   for(let note of noteTraits){ //draws notes
     image(boom, windowWidth/2-spacing/2 + spacing / lanes * note.lane + offsetx + spacing/2/lanes, note.distance, 60, 60);
   }
 
   for(let note of noteTraits){ //does stuff that affects all notes
+    note.distance += note.speed; //moves notes
+  }
 
-  
-    
-    for(let idfk = 0; idfk < noteSpeed; idfk++){ //this moves the notes
-      noteDistance[count]++;
-    }
-
-    if(note.distance >= windowHeight){ //kills notes keep an eye on this
-      noteTraits.splice[note-1, 1];
+  for (let i = 0; i < noteTraits.length; i++){//kills notes keep an eye on this
+    if(noteTraits[i].distance >= windowHeight){ 
+      noteTraits.splice(i, 1);
     }
   }
 }
@@ -151,14 +149,8 @@ function spawnNote() {
   noteTraits.push(tempNote);
 }
 
-function candyMan(){ //short for eyecandy manager, this is the function i will use to make everything visually distracting :)
-  //spacing = mouseX - windowWidth/2;
-  line(windowWidth/2, 0, windowWidth/2, windowHeight);
+function candyMan(){ //short for eyecandy manager, this is the function i will use to make everything visually distracting
 
-
-  // translate(100,100);
-  // rotate(mouseX);
-  // square(0, 0, 100);
-  // pop();
-  //rotating rotates the grid, not an individual object
 }
+
+//im also gonna have to make a separate draw function for notes and stuff so that I can rotate them and stuff
